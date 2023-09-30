484088a148b5df07a2aef89222de78411ad44777
FabianaMP <fmarquespenteado@gmail.com>
2023-09-29 22:41:43 -0300

    first commit

---
+++ src/main/java/com/FI/EscolaOn/EscolaOnApplication.java
@@ -0,0 +1,13 @@
+package com.FI.EscolaOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscolaOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscolaOnApplication.class, args);
+	}
+
+}
